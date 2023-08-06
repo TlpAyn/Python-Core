e418e12d79383a62fae88eaa29795bd3f87bea13
talipa <talipayan6035@gmail.com>
2023-08-06 23:23:55 +0200

    classlara ve nesneye gecildi

---
+++ mapList.py
@@ -0,0 +1,5 @@
+liste =[10344,9,8,7,7,6,5,4]
+
+neu_list = list(map(lambda x: x**2,liste))
+
+print(neu_list)
---
+++ packingAndUnpacking.py
@@ -0,0 +1,10 @@
+def argsCalismaOrnegi (*args):
+    for element in args:
+        print (element)
+
+
+sayilar =[1,2,3,4,5,6,7]
+
+x= argsCalismaOrnegi(sayilar)
+
+print(x)
